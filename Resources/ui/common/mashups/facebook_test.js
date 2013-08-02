9f0e15b086a8bd791a74928e62d318e1f0a7f65f
Kaspars Primaks <kaspars@boxlightmedia.com>
2013-08-02 18:10:12 +0100

    fixed facebook album functionality for android devices

--- Resources/ui/common/mashups/facebook_test.js
+++ Resources/ui/common/mashups/facebook_test.js
@@ -17,24 +17,25 @@ function facebook_test(_args) {
 			test : 'ui/common/mashups/facebook_query'
 		},
 		{
-			title : 'Albums',
-			hasChild : true,
-			test : 'ui/common/mashups/facebook_albums'
-		},
-		{
 			title : 'Publish Stream',
 			hasChild : true,
 			test : 'ui/common/mashups/facebook_publish_stream'
 		}
-	/*
-	, {
-		title : 'Photos',
-		hasChild : true,
-		test : 'ui/common/mashups/facebook_photo_upload'
-	}
-	*/
 	];
-
+	
+	var osname = Ti.Platform.osname;
+	var albumsWin = {
+		title : 'Albums',
+		hasChild : true
+	};
+	if (osname === 'iphone' || osname === 'ipad') {
+		albumsWin.test = 'ui/common/mashups/facebook_albums';
+	} else {
+		albumsWin.test = 'ui/common/mashups/facebook_albums_android';	
+	}
+	
+	data.push(albumsWin);
+	
 	// create table view
 	for (var i = 0; i < data.length; i++) {
 		data[i].color = '#000';
